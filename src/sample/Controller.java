79dd6c337aa22ba0f047e8c52e95fbb7a2dd1783
cruxeat <32929882+cruxeat@users.noreply.github.com>
2017-11-09 13:41:48 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField textfield1;
+    public TextField textfield2;
+    public Label label;
+
+
+
+    public void handleCLICK(ActionEvent actionEvent) {
+        label.setText(textfield1.getText()+textfield2.getText());
+    }
+
+}
